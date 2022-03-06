2b7fcf1f3b176ab00480479a3e1dd7f212a37ffc
Dau-Truong-Quan <quansonvu2408@gmail.com>
2022-03-06 14:53:23 +0700

    first commit

---
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> arrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+            listView = (ListView) findViewById(R.id.listviewMonHoc);
+            arrayList = new ArrayList<>();
+
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+    }
+}
